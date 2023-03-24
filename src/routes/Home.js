17409c2f3c1abcaf80499793ab12c36d46ac31d7
eunseolYun <skopdt73@gmail.com>
2023-03-24 17:56:22 +0900

    import firestore create collection, docs create nweets and read it(getDocs) deleted StrictMode in index.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "fbase";
+import { collection, addDoc, getDocs, query } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const q = query(collection(dbService, "nweets"));
+        // collection의 모든 nweet을 data()를 사용해 내용을 가져옴
+        const querySnapshot = await getDocs(q);
+        // set함수를 쓸 때 값 대신 이전값에 접근할 수 있는 함수를 전달 할 수 있다.
+        querySnapshot.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getNweets();
+    }, []);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await addDoc(collection(dbService, "nweets"), {
+            nweet,
+            createdAt: Date.now(),
+        });
+        setNweet("");
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNweet(value);
+    };
+    return (
+        <div>
+            <form>
+                <input
+                    type="text"
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                    value={nweet}
+                    onChange={onChange}
+                />
+                <input type="submit" value="Nweet" onClick={onSubmit} />
+            </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 export default Home;
